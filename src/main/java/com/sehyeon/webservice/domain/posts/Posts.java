5bab56a19069a87b5ad87d96c3221199709e319d
sehyeon_kim <shgim9977@gmail.com>
2022-03-22 18:53:16 +0900

    [add] 생성일, 수정일 추가(BaseTimeEntity 상속)

--- src/main/java/com/sehyeon/webservice/domain/posts/Posts.java
+++ src/main/java/com/sehyeon/webservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.sehyeon.webservice.domain.posts;
 
+import com.sehyeon.webservice.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)//기본 생성자 자동 추가, 접근권한 protected. 프로젝트 코드상에서 기본생성자로 생성하는 것은 막되, JPA에서 Entity 클래스를 생성하는것은 허용
 @Getter//클래스내 모든 필드의 Getter 메소드를 자동생성
 @Entity//테이블과 링크될 클래스, 언더스코어 네이밍
-public class Posts {//Posts 클래스는 실제 DB의 테이블과 매칭될 클래스이며 보통 Entity클래스
+public class Posts extends BaseTimeEntity {//Posts 클래스는 실제 DB의 테이블과 매칭될 클래스이며 보통 Entity클래스
 
     //JPA에서 제공하는 어노테이션
     @Id//해당 테이블 PK필드
--- src/test/java/com/sehyeon/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/sehyeon/webservice/domain/posts/PostsRepositoryTest.java
@@ -8,11 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 //import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -42,5 +44,24 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo("테스트 게시글");
         assertThat(posts.getContent()).isEqualTo("테스트 본문");
+    }
+
+    @Test
+    public void BaseTimeEntity_등록 () {
+        //given
+        LocalDateTime now = LocalDateTime.now();
+        postsRepository.save(Posts.builder()
+                .title("테스트 게시글")
+                .content("테스트 본문")
+                .author("shgim9977@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertTrue(posts.getCreatedDate().isAfter(now));
+        assertTrue(posts.getModifiedDate().isAfter(now));
     }
 }
